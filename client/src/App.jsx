69a8907b38877ac14b89a7a6955cad1852cbf5df
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2025-05-08 15:25:24 +0530

    tailwindsetup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,9 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <div className='bg-red-700'>Madhukr kumar...</div>
   )
 }
 
