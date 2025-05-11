ce8aeeb74e158ff367220f4d4fe373f8e4d19153
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2025-05-11 18:32:39 +0530

    HabitatCardformation

--- client/src/components/Habitat.jsx
+++ client/src/components/Habitat.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import HeroHabitat from './Herosection/HeroHabitat'
+import HabitatCard from './HabitatCard'
 
 const Habitat = () => {
   return (
     <div>
       <Navbar/>
       <HeroHabitat/>
+      <HabitatCard/>
     </div>
   )
 }
