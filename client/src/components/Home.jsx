17510e18f718289838e4e71fef2937169aca35d0
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2025-05-14 17:40:14 +0530

    merge

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
 import HeroHome from './Herosection/HeroHome'
-import HomeChart from './HomeChart'
-import HomeTable from './HomeTable'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <HeroHome/>
-      <HomeChart/>
-      <HomeTable/>
     </div>
   )
 }
